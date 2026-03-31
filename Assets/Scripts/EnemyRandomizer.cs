f6f4cc0d28550a015e9b6a9a3ca8a28728a5459b
Frances2345 <francesco.moreno@tecsup.edu.pe>
2026-03-31 17:28:18 -0500

    xxx

---
+++ Assets/Scripts/EnemyRandomizer.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyRandomizer : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform player;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        agent.speed = Random.Range(3.0f, 6.0f);
+        agent.acceleration = Random.Range(7.0f, 15.0f);
+        agent.angularSpeed = Random.Range(120f, 250f);
+        agent.stoppingDistance = Random.Range(1.2f, 2.5f);
+        agent.avoidancePriority = Random.Range(0, 99);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            agent.SetDestination(player.position);
+        }
+    }
+}
